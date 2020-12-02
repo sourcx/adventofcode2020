726397dbbfc2de2d341c97c190cac5e4857494e9
sourcx <wh@y42.nl>
2020-12-02 10:59:32 +0100

    Day 1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # adventofcode
+
+From <https://docs.microsoft.com/en-gb/dotnet/core/get-started>
+
+dotnet new console --output sample1
+dotnet run --project sample1
---
+++ adventofcode2020/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace adventofcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1();
+        }
+
+        // Find the two entries that sum to 2020; what do you get if you multiply them together?
+        static void Day1()
+        {
+            Int16 YEAR = 2020;
+
+            var entries = new List<Int16>();
+
+            foreach (var line in File.ReadLines("input/1"))
+            {
+                entries.Add(Int16.Parse(line));
+            }
+
+            foreach (var one in entries)
+            {
+                foreach (var other in entries)
+                {
+                    if (one + other == YEAR)
+                    {
+                        Console.WriteLine($"{one} + {other} sums up to {YEAR}");
+                        Console.WriteLine($"Their product is {one * other}");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
