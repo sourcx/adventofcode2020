b1f6064a97110be65055e61f72017e5b50db9633
sourcx <wh@y42.nl>
2020-12-05 13:10:35 +0100

    Day 5

--- adventofcode2020/Program.cs
+++ adventofcode2020/Program.cs
@@ -17,7 +17,9 @@ namespace adventofcode
             // Day3();
             // Day3_part2();
             // Day4();
-            Day4_part2();
+            // Day4_part2();
+            // Day5();
+            Day5_part2();
         }
 
         // Find the two entries that sum to 2020; what do you get if you multiply them together?
@@ -329,6 +331,71 @@ namespace adventofcode
             }
 
             return true;
+        }
+
+        // What is the highest seat ID on a boarding pass?
+        static void Day5()
+        {
+            Console.WriteLine($"Highest seat ID: {GetAllSeatIDs().Max()}.");
+        }
+
+        static List<int> GetAllSeatIDs()
+        {
+            const int MAX_SEAT_ROW = 128;
+            const int MAX_SEAT_COL = 8;
+
+            var ids = new List<int>();
+
+            foreach (var line in File.ReadLines("input/5"))
+            {
+                var rowInstructions = line.Substring(0, 7);
+                int row = PartitionBinarySpace(rowInstructions, 'F', 'B', MAX_SEAT_ROW);
+
+                var colInstructions = line.Substring(7, 3);
+                int col = PartitionBinarySpace(colInstructions, 'L', 'R', MAX_SEAT_COL);
+
+                var seatId = MAX_SEAT_COL * row + col;
+                ids.Add(seatId);
+                // Console.WriteLine($"{line}: row {row}, col {col}, seat ID {seatId}.");
+            }
+
+            return ids;
+        }
+
+        static int PartitionBinarySpace(string instructions, char lowerHalf, char upperHalf, int max)
+        {
+            int lo = 0;
+            int hi = max;
+
+            foreach (char instruction in instructions)
+            {
+                if (instruction == lowerHalf)
+                {
+                    hi -= ((hi - lo) / 2);
+                }
+
+                if (instruction == upperHalf)
+                {
+                    lo += ((hi - lo) / 2);
+                }
+            }
+
+            return lo;
+        }
+
+        static void Day5_part2()
+        {
+            var bookedSeats = GetAllSeatIDs();
+            bookedSeats.Sort();
+
+            var allSeats = Enumerable.Range(bookedSeats.First(), bookedSeats.Last()).ToList();
+
+            // First one should be missing one
+            foreach (var seat in allSeats.Except(bookedSeats))
+            {
+                Console.WriteLine($"{seat} is my seat.");
+                return;
+            }
         }
     }
 }
